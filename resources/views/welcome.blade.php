e783190226b5fadc7e12efae18ba143c40b990af
nadws <nandw567@gmail.com>
2023-12-09 11:09:15 +0800

    dsa

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
+
+
+</head>
+
+<body>
+    <div
+        class="flex items-center justify-center  bg-gradient-to-b from-[#010203] via-[#07132C] to-[#041F4C] to-[#0B6FA8] p-36 h-screen">
+        <h1 class="text-9xl font-bold  text-white text-center font-Silkscreen">
+            The internet is
+            your canvas.
+        </h1>
+    </div>
+
+</body>
+
+</html>
