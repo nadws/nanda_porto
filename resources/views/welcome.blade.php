a0394539273a871b6f2c406351716448c441033e
nadws <nandw567@gmail.com>
2023-12-12 10:40:59 +0800

    dsa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400&display=swap" rel="stylesheet">
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
 
 
 </head>
