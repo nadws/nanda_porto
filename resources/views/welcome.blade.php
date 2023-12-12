652755afcda444907fcc88ca0176a94ab15c38c7
nadws <nandw567@gmail.com>
2023-12-12 10:42:30 +0800

    dsada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400&display=swap" rel="stylesheet">
-    {{-- @vite('resources/css/app.css') --}}
+    @vite('resources/css/app.css')
 
 
 </head>
@@ -17,7 +17,7 @@
         class="flex items-center justify-center  bg-gradient-to-b from-[#010203] via-[#07132C] to-[#041F4C] to-[#0B6FA8] p-36 h-screen">
         <h1 class="text-9xl font-bold  text-white text-center font-Silkscreen">
             The internet is
-            your canvas. sa
+            your canvas
         </h1>
     </div>
 
